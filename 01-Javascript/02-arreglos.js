bbd42bf366bdb070f437bb5d943e8422bb2f34d2
Katherin Sango <kate.liz40@gmail.com>
2020-06-16 17:59:56 -0500

    Variables y arreglos

---
+++ 01-Javascript/02-arreglos.js
@@ -0,0 +1,5 @@
+const arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; //no se puede reasignar valores
+const  arregloTodo = [
+    true, 1, 1.2, "Adrian", undefined, null, {}, [1,2,true,"A"]  //{} objeto vacio
+];
+
