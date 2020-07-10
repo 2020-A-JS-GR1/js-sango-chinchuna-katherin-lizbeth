736ebbe2a13d6187651d18c13b36b189e5b9a744
Katherin Sango <kate.liz40@gmail.com>
2020-07-10 17:51:08 -0500

    Variables e Interfaces en TS

---
+++ 02-typescript/01-variables.ts
@@ -0,0 +1,20 @@
+//para ejecutar desde consola usamos el comando node nombre_archivo
+let nombre:string = 'Katherin';
+// No nos permite hacer esto porque estamos usando Duck Typing
+let edad:number = 31;
+let casado:boolean = false;
+let fecha:Date = new Date();
+
+let marihuana: any = 2;
+marihuana = "2";
+marihuana = () => '2';
+
+let edadMultiple:number | string = 2;
+edadMultiple = '2';
+edadMultiple = 2222;
+edadMultiple = 'dos';
+
+
+
+
+
