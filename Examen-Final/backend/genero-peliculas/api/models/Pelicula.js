9d00a856c033aa544ca9ec3c6dd7a8e9b59229df
Katherin Sango <kate.liz40@gmail.com>
2020-09-16 03:17:17 -0500

    Examen - creacion entidades

---
+++ Examen-Final/backend/genero-peliculas/api/models/Pelicula.js
@@ -0,0 +1,59 @@
+/**
+ * Pelicula.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  datastore: 'default',
+  tableName: 'db_pelicula',
+  attributes: {
+
+    codigo:{
+      type: 'string',
+      columnName: 'codigo_pelicula',
+      unique: true,
+      minLength: 3,
+      maxLength: 6,
+      required: true, // Por defecto es false
+    },
+
+    nombre:{
+      type: 'string',
+      columnName: 'nombre_pelicula',
+      minLength: 3,
+      required: true,
+    },
+
+    fechaEstreno:{
+      type: 'string',
+      columnName: 'fecha_estreno',
+      columnType: 'Date',  //año-mes-dia 2020/10/15
+      maxLength: 15,
+    },
+
+    estrenada:{
+      type: 'boolean',
+      columnName: 'estrenada',
+      //columnType: 'Bool', //0 ó 1
+    },
+
+    duracion:{
+      type: 'number',
+      columnName: 'duracion',
+      columnType: 'integer',
+    },
+
+    //RELACIONES
+    //Pelicula <- Genero
+    genero:{ //Nombre FK
+      model: 'Genero',
+      required: true,   //Requerida 1 -N
+    }
+
+  },
+
+};
+
